409fee12d4a809873db162cf600d665b8351f2ce
jeffersonrolino <jefferson.rolino@gmail.com>
2024-02-14 16:42:55 -0300

    Seção 02: Express e primeiras rotas - Aula 08: Deletando livros

--- src/app.js
+++ src/app.js
@@ -31,17 +31,23 @@ app.get("/livros", (req, res) => {
 app.get("/livros/:id", (req, res) => {
     const index = buscaLivro(req.params.id);
     res.status(200).json(livros[index]);
-})
+});
 
 app.put("/livros/:id", (req, res) => {
     const index = buscaLivro(req.params.id);
     livros[index].titulo = req.body.titulo;
     res.status(200).json(livros);
-})
+});
 
 app.post("/livros", (req, res) => {
     livros.push(req.body);
     res.status(201).send("livro cadastrado com sucesso");
-})
+});
+
+app.delete("/livros/:id", (req, res) => {
+    const index = buscaLivro(req.params.id);
+    livros.splice(index, 1);
+    res.status(200).send("Livro removido com sucesso");
+});
 
 export default app;
