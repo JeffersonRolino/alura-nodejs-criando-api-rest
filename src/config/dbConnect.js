34018403ef1bec2d96344c77ec56a318b4595f78
jeffersonrolino <jefferson.rolino@gmail.com>
2024-02-21 23:38:16 -0300

    Seção 03: Persistindo dados - Aula 10: Acessando a coleção livros

--- src/config/dbConnect.js
+++ src/config/dbConnect.js
@@ -1,10 +1,7 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 async function connectToDataBase(){
-    mongoose.connect(process.env.DB_KEY);
+    mongoose.connect(process.env.DB_CONNECTION_STRING);
 
     return mongoose.connection;
 };
